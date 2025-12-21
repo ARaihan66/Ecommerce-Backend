2729a8804a0492cd7db4e1e608f2a01b0edf9fc8
Abu Raihan <aburaihancse066@gmail.com>
2025-12-21 16:41:18 +0600

    added login router

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { SignUpUser } from "../controllers/UserController.js";
+import { SignInUser, SignUpUser } from "../controllers/UserController.js";
 import upload from "../middleware/upload.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", upload.single("profilePic"), SignUpUser);
+userRouter.post("/signin", SignInUser);
 
 export default userRouter;
